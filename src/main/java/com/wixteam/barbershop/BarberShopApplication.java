3ad4dfe07112a728763adb8679ecd3e74b84d1dc
juan rincon <juanhit471523@gmail.com>
2022-04-25 18:35:32 -0500

    se inserta usuario

--- src/main/java/com/wixteam/barbershop/BarberShopApplication.java
+++ src/main/java/com/wixteam/barbershop/BarberShopApplication.java
@@ -17,17 +17,7 @@ public class BarberShopApplication {
         sApp.run(args);
 
 
-        System.out.printf("hello");
-        Connection connection=null;
-        connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/prueba?user=root&password=12345");
-        Statement statement = connection.createStatement();
-        statement.setQueryTimeout(30);
-        ResultSet rs = statement.executeQuery("select* from author");
 
-        while (rs.next()){
-            System.out.println("Autor: "+ rs.getString("namee" ) + "   Apellido: " + rs.getString("last_name") );
-        }
-       // System.out.println("nombre" + rs.getString("namee"));
     }
 
 }
