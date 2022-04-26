acc3c48bc878564ef23a96e8b6aedd4e25f2b363
AndresFTA22 <andrest9822@gmail.com>
2022-04-26 02:26:35 -0500

    Services works

--- src/main/java/com/wixteam/barbershop/Services/Service/Infraestructure/Controllers/PutServiceController.java
+++ src/main/java/com/wixteam/barbershop/Services/Service/Infraestructure/Controllers/PutServiceController.java
@@ -1,2 +1,62 @@
-package com.wixteam.barbershop.Services.Service.Infraestructure.Controllers;public class PutServiceController {
+package com.wixteam.barbershop.Services.Service.Infraestructure.Controllers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wixteam.barbershop.Services.Service.Application.Update.ServiceUpdate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/Services")
+public class PutServiceController {
+    @Autowired
+    private ServiceUpdate update;
+    @PutMapping(value = "/Update/{ServiceId}")
+    public ResponseEntity execute(@PathVariable("ServiceId") String id, @RequestBody ServiceRequest request){
+        update.execute(id, request.getServicePrice(), request.getServiceDetails());
+        return ResponseEntity.status(HttpStatus.CREATED).body("Se modifico el servicio con id "+ id);
+    }
+
+    static class ServiceRequest {
+        @JsonProperty("serviceId")
+        private String serviceId ;
+        @JsonProperty("servicePrice")
+        private double servicePrice;
+        @JsonProperty("serviceDetails")
+        private String serviceDetails ;
+
+        @Override
+        public String toString() {
+            return "ServiceRequest{" +
+                    "serviceId='" + serviceId + '\'' +
+                    ", servicePrice='" + servicePrice + '\'' +
+                    ", serviceDetails='" + serviceDetails + '\'' +
+                    '}';
+        }
+
+        public String getServiceId() {
+            return serviceId;
+        }
+
+        public void setServiceId(String serviceId) {
+            this.serviceId = serviceId;
+        }
+
+        public double getServicePrice() {
+            return servicePrice;
+        }
+
+        public void setServicePrice(double servicePrice) {
+            this.servicePrice = servicePrice;
+        }
+
+        public String getServiceDetails() {
+            return serviceDetails;
+        }
+
+        public void setServiceDetails(String serviceDetails) {
+            this.serviceDetails = serviceDetails;
+        }
+    }
 }
--- src/main/java/com/wixteam/barbershop/Services/Service/Infraestructure/Hibernate/HibernateServiceRepository.java
+++ src/main/java/com/wixteam/barbershop/Services/Service/Infraestructure/Hibernate/HibernateServiceRepository.java
@@ -2,6 +2,7 @@ package com.wixteam.barbershop.Services.Service.Infraestructure.Hibernate;
 
 import com.wixteam.barbershop.Services.Service.Domain.Ports.ServiceRepository;
 import com.wixteam.barbershop.Services.Service.Domain.Service;
+import com.wixteam.barbershop.Services.Service.Domain.ValueObjects.ServiceId;
 import com.wixteam.barbershop.Shared.Infraestructure.Hibernate.HibernateRespository;
 import com.wixteam.barbershop.Users.User.Domain.User;
 import org.hibernate.SessionFactory;
@@ -17,21 +18,27 @@ public class HibernateServiceRepository extends HibernateRespository<Service> im
 
     @Override
     public void save(Service service) {
-
+        persist(service);
     }
 
     @Override
     public List<Service> all() {
-        return null;
+        return getAll().get();
     }
 
     @Override
     public void delete(String idService) {
-
+        Service service = byId(new ServiceId(idService)).get();
+        deleteEntity(service);
     }
 
     @Override
-    public void update(Service service, String idService) {
-
+    public void update(Service pService, String idService) {
+        Service service = byId(new ServiceId(idService)).get();
+        if(pService.getServiceDetails().value()!=null)
+            service.setServiceDetails(pService.getServiceDetails());
+        if(pService.getServicePrice().value()!=0)
+            service.setServicePrice(pService.getServicePrice());
+        updateEntity(service);
     }
 }
